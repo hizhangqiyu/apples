fcb323106d0bbfe9ae76f466441c73bd38e5d546
AppleFairy <zqyjixiang89@gmail.com>
2016-12-26 14:27:57 +0800

    print color for perl programming model

---
+++ print_color.pl
@@ -0,0 +1,19 @@
+#!/bin/env perl
+use strict;
+#Set Color
+my $black         = "\033[0;30m";
+my $dary_gray     = "\033[1;30m";    my $light_gray   = "\033[0;37m";
+my $blue          = "\033[0;34m";    my $light_blue   = "\033[1;34m";
+my $green         = "\033[0;32m";    my $light_green  = "\033[1;32m";
+my $cyan          = "\033[0;36m";    my $ligth_cyan   = "\033[1;36m";
+my $red           = "\033[0;31m";    my $ligth_red    = "\033[1;31m";    my $red_bold = "\033[01;31m";
+my $purple        = "\033[0;35m";    my $light_purple = "\033[1;35m";
+my $brown         = "\033[0;33m";
+my $yellow        = "\033[1;33m";
+my $white         = "\033[1;37m";
+my $default_color = "\033[00m";
+my $end           = "\033[0m";
+
+print $blue  ."asdfasfasfas$end\n";
+print $red   ."qwerqwerqwrqwer$end\n";
+print $yellow."zxcvzxvcv$end\n";
