1e018027076823a40044859b6e2923d15cbfd489
QiYu <zqyjixiang89@gmail.com>
2019-04-01 19:07:55 +0800

    xiaozhangyu

---
+++ coding/xiaozhangyu.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+std::vector<float> result;
+std::vector<float> indices;
+bool isSumZero(const std::vector<float>& v)
+{
+    float sum = 0;
+    for(auto n : v)
+    {
+        sum += n;
+    }
+    return (sum == 0 && !v.empty());
+}
+
+void findSumZero(const std::vector<float>& in, float i)
+{
+    if(i < 0)
+        return;
+
+    if(isSumZero(result))
+    {
+        for(auto v : result)
+            std::cout << std::fixed << std::setprecision(2) << v << ",";
+        std::cout << std::endl;
+    }
+    result.push_back(in[i]);
+    findSumZero(in, i-1);
+    result.pop_back();
+    findSumZero(in, i-1);
+}
+
+int main()
+{
+    std::ifstream input;
+    input.open("input.csv", std::ios::in);
+    std::string line;
+    float value;
+    std::vector<float> in;
+    while(getline(input, line))
+    {
+        std::stringstream ss(line);
+        ss >> value;
+        in.push_back(value);
+    }
+    for(auto v : in)
+        std::cout << v << " ";
+    std::cout << std::endl;
+    std::cout << "Total=" << in.size() << std::endl;
+    findSumZero(in, in.size() - 1);
+
+    std::ofstream output;
+    output.open("output.csv", std::ios::out);
+
+    return 0;
+}
